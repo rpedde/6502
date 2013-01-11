401098029012a9c6c9acc370ede20def6b814c8a
rpedde <ron@pedde.com>
2013-01-10 23:54:55 -0600

    simple input

--- os/ron-test.asm
+++ os/ron-test.asm
@@ -5,33 +5,60 @@
 
 uart_base = $1000
 
-        .org $e000
+temp_reg = $0f
 
-reset_routine
-        ldx #$00
+                .org $e000
 
-        lda output,x
+reset_routine:  ldy #o_start & $00ff
+                lda #o_start > 8
+                jsr ser_outstring
 
-outloop sta uart_base
-        inx
-        lda output,x
-        bne outloop
-        jmp *
+prompt:         lda #'>'
+                sta uart_base
+                lda #' '
+                sta uart_base
 
-irq_routine
-nmi_routine
-        rti
+poll_uart:      lda uart_base + 5
+                and #$01
+                beq poll_uart
+
+                lda uart_base
+                sta uart_base
+
+                cmp #$0d
+                bne poll_uart
+
+                lda #$0a
+                sta uart_base
+                jmp prompt
+
+                ;; output a string, assumes A:Y points to zero terminated
+                ;; string.  destroys A and Y
+ser_outstring:  sty temp_reg
+                sta temp_reg + 1
+
+                ldy #$00
+
+ser_outstring1: lda (temp_reg),y
+                beq ser_outstring2
+
+                sta uart_base
+                iny
+                bne ser_outstring1
+
+ser_outstring2: rts
+
+
+irq_routine:
+nmi_routine:
+                rti
 
 
 
+o_start:        .db     "Howdy from 6502", $0a, $0d, $00
 
+                .org $fffa
 
-
-output  .db     "Testing 16550 UART"
-        .db     $00
-
-        .org $fffa
-
-v_nmi   dw nmi_routine
-v_reset dw reset_routine
-v_irq   dw irq_routine
+v_nmi:          dw nmi_routine
+v_reset:        dw reset_routine
+v_irq:          dw irq_routine
