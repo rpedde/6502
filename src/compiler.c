6ca614090575551c54f61f34f470d7f6bd9cb660
rpedde <ron@pedde.com>
2013-01-26 23:30:14 -0600

    add gpl license headers to files

--- src/compiler.c
+++ src/compiler.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Ron Pedde (ron@pedde.com)
+ * Copyright (C) 2009-2103 Ron Pedde (ron@pedde.com)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/debuginfo.c
+++ src/debuginfo.c
@@ -1,5 +1,4 @@
-/* -*- mode: c -*-
- *
+/*
  * Copyright (C) 2013 Ron Pedde <ron@pedde.com>
  *
  * This program is free software: you can redistribute it and/or modify
